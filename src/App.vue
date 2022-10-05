5e575fca8679eeebc01f31d38535d13cdac18a6a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-10-05 14:42:24 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<header class="flex justify-center align-center shadow-md p-5 bg-sky-500">
+  <router-link to="/">
+    <h1 class="text-xl font-bold text-sky-50"><span class="decoration-white pr-2">Movie</span>App</h1>
+  </router-link>
+</header>
+<main>
+  <router-view />
+</main>
+  
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
   }
-}
+  body {
+    background-color: rgb(3 105 161);
+  }
 </style>
